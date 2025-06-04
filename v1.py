0b285e5d08ac8a6f110db2b246542e031321bd3c
antoninawlo <antoninawlodarczyk55@gmail.com>
2025-06-04 20:00:53 +0200

    dodanie sygnalow

--- v1.py
+++ v1.py
@@ -42,17 +42,50 @@ while wart != 1 and wart != 2:
         
         print(f"J1 = {J1}, J2 = {J2}, k = {k}, b = {b}, n1 = {n1}, n2 = {n2}")
         
-def model(t, x, Tm):
-    dx1 = x[1]
-    dx2 = (Tm*n2/n1 - b* x[1] - k*n2/n1* x[0]) / (J1*(n2/n1)**2 + J2)
+x0 = 0
+dt = 0.01
+t = 10
+t_krok = np.arange(x0, t, dt)  
+        
+wart = 0
+print("Wybierz sygnal wejsciowy: 1 - prostokatny, 2 - trojkatny, 3 - harmoniczny")
+wart = int(input("Wybor: "))
+
+while wart != 1 and wart != 2 and wart != 3:
+    wart = int(input("Wybor: "))
+    f = 5
+
+    if wart == 1:
+        A = input("Amplituda = ")
+        Tm = A * signal.square(2 * np.pi * f * t_krok, width = 0.5)
+        
     
-    return np.array([dx1, dx2])
+    if wart == 2:
+        A = input("Amplituda = ")
+        Tm = A * signal.sawtooth(2 * np.pi * t_krok)
+        
+        
+    if wart == 3:
+        A = input("Amplituda = ")
+        Tm = A * np.sin(2 * np.pi * f * t_krok)
+        
+        
+
+def model(t, theta, Tm):
+    dtheta1 = theta[1]
+    dtheta2 = (Tm*n2/n1 - b* theta[1] - k*n2/n1* theta[0]) / (J1*(n2/n1)**2 + J2)
+    y = theta[0]
+    
+    return np.array([dtheta1, dtheta2, y])
 
 
 #%%
 
+x0 = 0
+dt = 0.01
+t = 10
+t_krok = np.arange(x0, t, dt)  
 
-    
-
+def euler(funkcja, x0, t, u):
     
 

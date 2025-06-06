ec5fe18db75ad7a3627174571c9cef8418c84af9
antoninawlo <antoninawlodarczyk55@gmail.com>
2025-06-06 21:57:59 +0200

    bez wyboru parametrow

---
+++ v2.py
@@ -0,0 +1,104 @@
+#%% 
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+import random as ra
+import os
+
+results_dir = "results"
+os.makedirs(results_dir, exist_ok = True)
+
+print("Importy gotowe")
+
+
+#%%
+
+J1 = 0.05
+J2 = 0.1
+k = 10
+b = 1
+n1 = 1
+n2 = 2
+
+print(f"J1 = {J1}, J2 = {J2}, k = {k}, b = {b}, n1 = {n1}, n2 = {n2}")
+print("\n")
+
+theta0 = 0
+dt = 0.01
+t = 10
+t_krok = np.arange(0, t, dt)  
+
+#%%
+        
+wart = 0
+print("Wybierz sygnal wejsciowy: 1 - prostokatny, 2 - trojkatny, 3 - harmoniczny")
+wart = 0
+f = 5
+Tm = []
+
+while wart != 1 and wart != 2 and wart != 3:
+    wart = int(input("Wybor: "))
+
+    if wart == 1:
+        A = int(input("Amplituda = "))
+        #Tm = A * signal.square(2 * np.pi * f * t_krok)
+        
+        for i in range(len(t_krok)):
+            
+            if (i // (f * 2)) % 2 == 0:
+                Tm.append(A) 
+            else:
+                Tm.append(0)
+        
+    if wart == 2:
+        A = int(input("Amplituda = "))
+        Tm = A * signal.sawtooth(2 * np.pi * f * t_krok, width = 0.5)
+           
+    if wart == 3:
+        A = int(input("Amplituda = "))
+        Tm = A * np.sin(2 * np.pi * f * t_krok)
+        
+        
+def model(x, Tm):
+    dx1 = x[1]
+    dx2 = (Tm*n2/n1 - b* x[1] - k*n2/n1* x[0]) / (J1*(n2/n1)**2 + J2)
+    # theta1 = x[0]
+    
+    return np.array([dx1, dx2])
+
+
+#%%
+
+def euler(Tm): 
+    x = [0, 0]
+    lista_dx1 = []
+    lista_dx2 = []
+    
+    for i in range(len(t_krok)):
+        u = Tm[i]
+        dx1, dx2 = model(x, u)
+        x[0] = x[0] + dx1 * dt
+        x[1] = x[1] + dx2 * dt
+        
+        lista_dx1.append(x[0])
+        lista_dx2.append(x[1])
+        
+    return  lista_dx1, lista_dx2
+           
+           
+plt.plot(t_krok, Tm)
+plt.show
+
+#%%
+
+lista_dx1, lista_dx2 = euler(Tm) 
+
+plt.plot(t_krok, lista_dx1)
+plt.plot(t_krok, lista_dx2)
+plt.grid()
+plt.show()
+    
+
+
+# %%
